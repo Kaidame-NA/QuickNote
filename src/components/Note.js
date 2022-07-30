71029b32cdc52913d6539ab45b5509f22150d85b
Edward Wang <ewang43@jhu.edu>
2022-07-30 16:21:08 -0400

    Make notes expandable

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,38 @@
-import {ListItem, ListItemText} from "@material-ui/core";
-function Note(props) {
-    const { note } = props;
-    return (
-        <ListItem>
-            <ListItemText primary={note.title} secondary = {note.text} />
-        </ListItem>
-    )
+import React, {Component} from "react";
+import { Collapse, List, ListItem, ListItemText, ListItemIcon } from "@material-ui/core";
+import { ExpandLess, ExpandMore } from "@material-ui/icons";
+
+class Note extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+        };
+    }
+
+    handleClick = () => {
+        this.setState({ open: !this.state.open });
+    };
+
+    render() {
+        const { note } = this.props;
+        const { open } = this.state;
+        return (
+            <>
+                <ListItem onClick={this.handleClick}>
+                    <ListItemIcon>
+                        {open ? <ExpandLess /> : <ExpandMore />}
+                    </ListItemIcon>
+                    <ListItemText primary={note.title} />
+                </ListItem>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItemText secondary={note.text} />
+                    </List>
+                </Collapse>
+            </>);
+    }
 }
+
 
 export default Note;
