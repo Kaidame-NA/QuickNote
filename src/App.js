348f271e80fe13b9aee9106c63539cada4bfafe5
Edward Wang <ewang43@jhu.edu>
2022-07-30 16:34:39 -0400

    Add a fab for adding notes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
-import { Container, List } from "@material-ui/core";
+import { Container, List, Fab, withStyles } from "@material-ui/core";
+import { Add } from "@material-ui/icons";
 import Note from "./components/Note";
 
+const styles = {
+  fab: {
+    position: 'absolute',
+    bottom: "2rem",
+    right: "2rem",
+  }
+}; 
 
 class App extends Component {
   constructor(props) {
@@ -44,9 +52,12 @@ class App extends Component {
             return <Note note={note} key={index} deleteNote={this.deleteNote} />;
           })}
         </List>
+        <Fab aria-label={"Add"} className={this.props.classes.fab}>
+          <Add />
+        </Fab>
       </Container>
     )
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
